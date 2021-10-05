92bd772e8adbb78170ca4588bf70d48730bc4980
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-10-05 15:19:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {useState,useEffect} from "react";
 import {BrowserRouter,Switch,Route} from "react-router-dom";
 import Card from "./Components/cards";
 import Context from "./context";
-import "./app.css";
+import "./App.css";
 
 
 
